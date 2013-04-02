059fc232e954b895395eda140ab4958f92f6d900
Patrick Stein <Patrick.Stein@krokodil.jinx.de>
2013-04-02 12:42:48 +0200

    updated to osxfuse 2.5.5

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+OSXFUSE="osxfuse-2.5.5"
+
 if [[ ! -e /usr/local/bin/brew ]];
 then 
 	ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
@@ -7,29 +9,29 @@ fi
 
 if [[ ! -d /usr/local/include/osxfuse ]];
 then
-	if [[ ! -e OSXFUSE-2.5.4.dmg ]];
+	if [[ ! -e $OSXFUSE ]];
 	then
-		curl -L 'https://github.com/downloads/osxfuse/osxfuse/OSXFUSE-2.5.4.dmg' -o OSXFUSE-2.5.4.dmg
+		curl -L "http://downloads.sourceforge.net/project/osxfuse/$OSXFUSE/$OSXFUSE.dmg?use_mirror=autoselect" -o $OSXFUSE.dmg
 	fi
 	
-	if [[ ! -e OSXFUSE-2.5.4.dmg ]];
+	if [[ ! -e $OSXFUSE.dmg ]];
 	then
-		echo "Can't find OSXFUSE-2.5.4.dmg"
+		echo "Can't find $OSXFUSE.dmg"
 		exit 1;
 	fi
 
-	hdiutil attach OSXFUSE-2.5.4.dmg -nobrowse -mountpoint osxfuse
+	hdiutil attach $OSXFUSE.dmg -nobrowse -mountpoint osxfuse
 	
 	if [[ ! -e osxfuse/Install\ OSXFUSE\ 2.5.pkg ]];
 	then
-		echo "Can't find osxfuse/Install\ OSXFUSE\ 2.5.pkg"
+		echo "Can't find osxfuse/Install package"
 		exit 1;
 	fi
 	
 	echo "Installing OSXFuse requires admin privilges:"
 	sudo installer  -pkg osxfuse/Install\ OSXFUSE\ 2.5.pkg -target  /
 	hdiutil detach osxfuse
-	rm -f OSXFUSE-2.5.4.dmg
+	rm -f $OSXFUSE.dmg
 fi
 
 cp encfsmacosxfuse.rb /usr/local/Library/Formula/
